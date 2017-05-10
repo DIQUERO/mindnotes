1fc8de7f7bc630ad7fd301d579c226f2927e8e2f
cinthyasm0611 <cinsalma@gmail.com>
2017-05-10 14:33:46 -0600

    Modal vanilla.js

--- src/js/vainilla.js
+++ src/js/vainilla.js
@@ -1,10 +1,30 @@
-var notes = document.getElementsByClassName('note');
 var buttonDelete = document.getElementsByClassName('btn-delete');
-
+var notes = document.getElementsByClassName('note-block');
+var close = document.getElementsByClassName('close');
 
 //Add listener to button delete
 for (var i = 0; i < buttonDelete.length; i++) {
   buttonDelete[i].addEventListener('click', deleteNote);
+}
+
+for (var i = 0; i < notes.length; i++) {
+    notes[i].addEventListener('click',openNote);
+}
+
+for (var i = 0; i < close.length; i++) {
+    close[i].addEventListener('click',closeNote);
+}
+
+function openNote(){
+    this.classList.add('note-modal');
+    this.style.display = 'block';
+    this.childNodes[2].style.display = 'block';
+}
+
+function closeNote(e){
+  e.stopPropagation();
+  this.parentNode.parentNode.classList.remove('note-modal');
+  this.style.display = 'none';
 }
 
 function deleteNote(e) {
