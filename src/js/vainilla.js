79aa76dbfdc0607fb9213cef231f6a11ce021b3a
Dilan Quesada <di.quesada93@gmail.com>
2017-05-10 14:18:28 -0600

    Js: remove note functionality, aside and main-section styles changes, markup changed

--- src/js/vainilla.js
+++ src/js/vainilla.js
@@ -1,13 +1,13 @@
-var note = document.querySelector(".note");
+var notes = document.getElementsByClassName('note');
+var buttonDelete = document.getElementsByClassName('btn-delete');
 
-note.onclick = function() {
-    console.log(this.id);
+
+//Add listener to button delete
+for (var i = 0; i < buttonDelete.length; i++) {
+  buttonDelete[i].addEventListener('click', deleteNote);
 }
 
-articles = document.getElementsByTagName('article');
-for (var i = 0; i < articles.length; i++) {
-    articles[i].addEventListener('click',redirect);
-}
-function redirect(){
-    alert(this.id);
+function deleteNote(e) {
+  e.stopPropagation();
+  this.parentNode.parentNode.parentNode.remove();
 }
